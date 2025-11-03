e78223a946a137d5c42dac1413decd1742b045eb
Christian Vargas <vargas.christian@correounivalle.edu.co>
2025-11-02 21:30:57 -0500

    Inclusión de carpetas arquitectura MVVM y primera versión del gráfico de navegación

---
+++ app/src/main/java/com/univalle/inventoryapp/view/fragments/ItemDetailsFragment.kt
@@ -0,0 +1,20 @@
+package com.univalle.inventoryapp.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.univalle.inventoryapp.R
+
+class ItemDetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_details, container, false)
+    }
+
+
+}
