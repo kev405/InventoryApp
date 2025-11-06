a00540b7937281e481ae72e81ebfd8f43cb7d7aa
MoonLoveless <lexyls523@gmail.com>
2025-11-06 02:23:27 -0500

    feat(HU4): Implementa lógica de validación para botón 'Guardar' (Criterio 7)

--- app/src/main/java/com/univalle/inventoryapp/view/fragments/AddItemFragment.kt
+++ app/src/main/java/com/univalle/inventoryapp/view/fragments/AddItemFragment.kt
@@ -9,11 +9,22 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.univalle.inventoryapp.R
 import com.univalle.inventoryapp.databinding.FragmentAddItemBinding
+import android.text.Editable
+import android.text.TextWatcher
+import android.graphics.Color
+import android.graphics.Typeface
 
 class AddItemFragment : Fragment() {
 
-    // Variable para Data Binding
     private lateinit var binding: FragmentAddItemBinding
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable?) {
+            validarCampos()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,17 +34,37 @@ class AddItemFragment : Fragment() {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_add_item, container, false
         )
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
         binding.toolbarAddItem.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
 
+        binding.etCodigo.addTextChangedListener(textWatcher)
+        binding.etNombre.addTextChangedListener(textWatcher)
+        binding.etPrecio.addTextChangedListener(textWatcher)
+        binding.etCantidad.addTextChangedListener(textWatcher)
+    }
+
+    private fun validarCampos() {
+        val codigo = binding.etCodigo.text.toString()
+        val nombre = binding.etNombre.text.toString()
+        val precio = binding.etPrecio.text.toString()
+        val cantidad = binding.etCantidad.text.toString()
+        val estanLlenos = codigo.isNotBlank() && nombre.isNotBlank() &&
+                precio.isNotBlank() && cantidad.isNotBlank()
+
+        binding.btnGuardar.isEnabled = estanLlenos
+
+        if (estanLlenos) {
+            binding.btnGuardar.setTextColor(Color.WHITE)
+            binding.btnGuardar.setTypeface(null, Typeface.BOLD)
+        } else {
+            binding.btnGuardar.setTextColor(Color.LTGRAY)
+            binding.btnGuardar.setTypeface(null, Typeface.NORMAL)
+        }
     }
 }
